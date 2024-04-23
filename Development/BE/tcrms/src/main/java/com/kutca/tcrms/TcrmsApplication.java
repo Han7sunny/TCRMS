d46543e1f40a5d443ac1469f70e3cfa0788b4b9a
Han7sunny <xoa1235@naver.com>
2024-04-24 01:20:29 +0900

    [Feat]: Add Spring Initializr

---
+++ Development/BE/tcrms/src/main/java/com/kutca/tcrms/TcrmsApplication.java
@@ -0,0 +1,13 @@
+package com.kutca.tcrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcrmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcrmsApplication.class, args);
+	}
+
+}
---
+++ Development/BE/tcrms/src/test/java/com/kutca/tcrms/TcrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutca.tcrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
