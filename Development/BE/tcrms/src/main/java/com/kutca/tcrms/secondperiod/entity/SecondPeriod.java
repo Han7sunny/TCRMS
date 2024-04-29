99f620a5f10f2fa91b7f22f911772d7cefb16d8e
Han7sunny <xoa1235@naver.com>
2024-04-30 00:06:33 +0900

    [BE][Feat]: Add SecondPeriod Entity

---
+++ Development/BE/tcrms/src/main/java/com/kutca/tcrms/secondperiod/entity/SecondPeriod.java
@@ -0,0 +1,21 @@
+package com.kutca.tcrms.secondperiod.entity;
+
+import com.kutca.tcrms.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SecondPeriod {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long secondPeriodId;
+
+    private Boolean isSecondApply;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", unique = true)
+    private User user;
+}
