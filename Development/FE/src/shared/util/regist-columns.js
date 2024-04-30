86a9ff20db23eb5b73fb900d9a4d1a6be1fe69ec
seoyeong <snu8081@naver.com>
2024-04-30 17:41:41 +0900

    [FE][Feat]: regist-individual columns format created(regist-columns.js)

---
+++ Development/FE/src/shared/util/regist-columns.js
@@ -0,0 +1,11 @@
+// individual
+export const TABLE_COLUMNS_REGIST_INDIVIDUAL = [
+    {id: 0, type: 'text'},
+    {id: 1, name: '성명',type: 'input'},
+    {id: 2, name: '성별',type: 'radio'},
+    {id: 3, name: '외국인',type: 'checkbox&dropdown'},
+    {id: 4, name: '주민등록번호',type: 'input'},
+    {id: 5, name: '종목',type: 'checkbox'},
+    {id: 6, name: '체급(겨루기만)',type: 'dropdown'},
+    {id: 7, type: 'button'},
+]
