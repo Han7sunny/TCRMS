bedc01ebab84b9d7727cd22e2319c7ede7d71590
Han7sunny <xoa1235@naver.com>
2024-05-01 23:42:35 +0900

    [BE][Feat]: Add UniversityApplication Entity

---
+++ Development/BE/tcrms/src/main/java/com/kutca/tcrms/universityapplication/entity/UniversityApplication.java
@@ -0,0 +1,26 @@
+package com.kutca.tcrms.universityapplication.entity;
+
+import com.kutca.tcrms.event.entity.Event;
+import com.kutca.tcrms.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UniversityApplication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long universityApplicationId;
+
+    private int teamCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
