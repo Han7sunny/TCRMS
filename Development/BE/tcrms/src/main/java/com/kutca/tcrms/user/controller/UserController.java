471bb2dd6d2fa62feea4272fe8191444a91553f4
Han7sunny <xoa1235@naver.com>
2024-05-09 17:09:05 +0900

    [BE][Feat]: Add Swagger Annotation

--- Development/BE/tcrms/src/main/java/com/kutca/tcrms/user/controller/UserController.java
+++ Development/BE/tcrms/src/main/java/com/kutca/tcrms/user/controller/UserController.java
@@ -4,6 +4,11 @@ import com.kutca.tcrms.common.dto.response.ResponseDto;
 import com.kutca.tcrms.user.controller.dto.request.ChangePwRequestDto;
 import com.kutca.tcrms.user.controller.dto.request.LoginRequestDto;
 import com.kutca.tcrms.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +17,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "UserController")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "로그인")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = ResponseDto.class)))
     @PostMapping("/api/login")
     public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequestDto){
         try {
@@ -28,6 +36,7 @@ public class UserController {
         }
     }
 
+    @Operation(summary = "초기 비밀번호 변경", description = "isFirstLogin이 true일 경우에만 비밀번호 변경")
     @PostMapping("/api/changePW")
     public ResponseEntity<?> changePassword(@RequestBody ChangePwRequestDto changePwRequestDto) {
         try {
