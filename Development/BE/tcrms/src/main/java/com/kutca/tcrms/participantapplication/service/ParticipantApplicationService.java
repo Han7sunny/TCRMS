b3fb1d1a13c7a4bf19cc85045b1bbc338a408811
Han7sunny <xoa1235@naver.com>
2024-06-19 00:04:15 +0900

    [BE][Feat]: getParticipantApplicationFeeInfo 여러 메서드로 분리

--- Development/BE/tcrms/src/main/java/com/kutca/tcrms/participantapplication/service/ParticipantApplicationService.java
+++ Development/BE/tcrms/src/main/java/com/kutca/tcrms/participantapplication/service/ParticipantApplicationService.java
@@ -48,7 +48,7 @@ public class ParticipantApplicationService {
     @Transactional(readOnly = true)
     public ResponseDto<?> getParticipantApplicationFeeInfo(Long userId){
 
-        //  개인전(1~4), 겨루기 단체전(5,7), 품새 단체전(6,8), 품새 페어(9)
+        //  개인전(1 ~ 4), 겨루기 단체전(5,7), 품새 단체전(6,8), 품새 페어(9)
 
         AtomicInteger individualCount = new AtomicInteger();
         Map<String, Set<Integer>> teamCount = new HashMap<>();
@@ -61,81 +61,66 @@ public class ParticipantApplicationService {
 
             Long participantId = participant.getParticipantId();
 
-            if(participant.getGender().equals("여성")){
-
-                //  개인전
-                individualCount.addAndGet(participantApplicationRepository.countAllByParticipant_ParticipantIdAndEvent_EventIdBetween(participantId, 1L, 2L));
-
-                //  겨루기 단체전
-                List<ParticipantApplication> findParticipantApplications1 = participantApplicationRepository.findAllByParticipant_ParticipantIdAndAndEvent_EventId(participantId, 5L);
-                for (ParticipantApplication participantApplication : findParticipantApplications1) {
-                    teamCount.get("겨루기 단체전").add(participantApplication.getEventTeamNumber());
-                }
-                
-                //  품새 단체전
-                List<ParticipantApplication> findParticipantApplications2 = participantApplicationRepository.findAllByParticipant_ParticipantIdAndAndEvent_EventId(participantId, 6L);
-                for (ParticipantApplication participantApplication : findParticipantApplications2) {
-                    teamCount.get("품새 단체전").add(participantApplication.getEventTeamNumber());
-                }
+            if("여성".equals(participant.getGender())){
+                countIndividualParticipantApplications(participantId, individualCount, 1L, 2L);
+                countTeamParticipantApplications(participantId, teamCount, 5L, 6L);
             }
 
-            if(participant.getGender().equals("남성")){
-
-                //  개인전
-                individualCount.addAndGet(participantApplicationRepository.countAllByParticipant_ParticipantIdAndEvent_EventIdBetween(participantId, 3L, 4L));
-
-                //  겨루기 단체전
-                List<ParticipantApplication> findParticipantApplications1 = participantApplicationRepository.findAllByParticipant_ParticipantIdAndAndEvent_EventId(participantId, 7L);
-                for (ParticipantApplication participantApplication : findParticipantApplications1) {
-                    teamCount.get("겨루기 단체전").add(participantApplication.getEventTeamNumber());
-                }
-
-                //  품새 단체전
-                List<ParticipantApplication> findParticipantApplications2 = participantApplicationRepository.findAllByParticipant_ParticipantIdAndAndEvent_EventId(participantId, 8L);
-                for (ParticipantApplication participantApplication : findParticipantApplications2) {
-                    teamCount.get("품새 단체전").add(participantApplication.getEventTeamNumber());
-                }
-
+            if("남성".equals(participant.getGender())){
+                countIndividualParticipantApplications(participantId, individualCount, 3L, 4L);
+                countTeamParticipantApplications(participantId, teamCount, 7L, 8L);
             }
 
-            //  품새 페어
             List<ParticipantApplication> findParticipantApplications = participantApplicationRepository.findAllByParticipant_ParticipantIdAndAndEvent_EventId(participantId, 9L);
-            for (ParticipantApplication participantApplication : findParticipantApplications) {
-                teamCount.get("품새 페어").add(participantApplication.getEventTeamNumber());
-            }
+            findParticipantApplications.forEach(participantApplication -> teamCount.get("품새 페어").add(participantApplication.getEventTeamNumber()));
 
         });
 
-        ParticipantApplicationResponseDto.firstPeriod participantApplicationInfo1 = ParticipantApplicationResponseDto.firstPeriod.builder()
-                .eventName("개인전")
-                .participantCount(individualCount.get())
-                .participantFee(individualCount.get() * eventRepository.findById(1L).get().getEventFee())
+        return createResponseDto(individualCount, teamCount);
+
+    }
+
+    private void countIndividualParticipantApplications(Long participantId, AtomicInteger individualCount, Long startEventId, Long endEventId){
+        individualCount.addAndGet(participantApplicationRepository.countAllByParticipant_ParticipantIdAndEvent_EventIdBetween(participantId, startEventId, endEventId));
+    }
+
+    private void addTeamParticipantApplicationNumbers(Long participantId, Set<Integer> teamSet, Long eventId){
+        List<ParticipantApplication> findParticipantApplications = participantApplicationRepository.findAllByParticipant_ParticipantIdAndAndEvent_EventId(participantId, eventId);
+        findParticipantApplications.forEach(participantApplication -> teamSet.add(participantApplication.getEventTeamNumber()));
+    }
+
+    private void countTeamParticipantApplications(Long participantId, Map<String, Set<Integer>> teamCount, Long sparringEventId, Long poomsaeEventId){
+        addTeamParticipantApplicationNumbers(participantId, teamCount.get("겨루기 단체전"), sparringEventId);
+        addTeamParticipantApplicationNumbers(participantId, teamCount.get("품새 단체전"), poomsaeEventId);
+    }
+
+    private ParticipantApplicationResponseDto.firstPeriod createParticipantApplicationInfo(String eventName, int participantCount, Long eventId){
+
+        int participantFee = eventRepository.findById(eventId).get().getEventFee();
+
+        return ParticipantApplicationResponseDto.firstPeriod.builder()
+                .eventName(eventName)
+                .participantCount(participantCount)
+                .participantFee(participantCount * participantFee)
                 .build();
 
-        ParticipantApplicationResponseDto.firstPeriod participantApplicationInfo2 = ParticipantApplicationResponseDto.firstPeriod.builder()
-                .eventName("겨루기 단체전")
-                .participantCount(teamCount.get("겨루기 단체전").size())
-                .participantFee(teamCount.get("겨루기 단체전").size() * eventRepository.findById(5L).get().getEventFee())
-                .build();
+    }
 
-        ParticipantApplicationResponseDto.firstPeriod participantApplicationInfo3 = ParticipantApplicationResponseDto.firstPeriod.builder()
-                .eventName("품새 단체전")
-                .participantCount(teamCount.get("품새 단체전").size())
-                .participantFee(teamCount.get("품새 단체전").size() * eventRepository.findById(6L).get().getEventFee())
-                .build();
+    private ResponseDto<?> createResponseDto(AtomicInteger individualCount, Map<String, Set<Integer>> teamCount){
 
-        ParticipantApplicationResponseDto.firstPeriod participantApplicationInfo4 = ParticipantApplicationResponseDto.firstPeriod.builder()
-                .eventName("품새 페어")
-                .participantCount(teamCount.get("품새 페어").size())
-                .participantFee(teamCount.get("품새 페어").size() * eventRepository.findById(9L).get().getEventFee())
-                .build();
+        List<ParticipantApplicationResponseDto.firstPeriod> participantApplicationInfos = Arrays.asList(
+                createParticipantApplicationInfo("개인전", individualCount.get(), 1L),
+                createParticipantApplicationInfo("겨루기 단체전", teamCount.get("겨루기 단체전").size(), 5L),
+                createParticipantApplicationInfo("품새 단체전", teamCount.get("품새 단체전").size(), 6L),
+                createParticipantApplicationInfo("품새 페어", teamCount.get("품새 페어").size(), 9L)
+                );
 
         return ResponseDto.builder()
                 .isSuccess(true)
                 .payload(
                         ParticipantApplicationsResponseDto.builder()
-                        .participantApplicationInfos(Arrays.asList(participantApplicationInfo1, participantApplicationInfo2, participantApplicationInfo3, participantApplicationInfo4))
-                        .build())
+                                .participantApplicationInfos(Collections.singletonList(participantApplicationInfos))
+                                .build())
                 .build();
     }
 }
