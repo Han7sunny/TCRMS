98dab09261ca967b185bc0753c771e7347eabece
Han7sunny <xoa1235@naver.com>
2024-06-19 23:50:18 +0900

    [BE][Feat]: Add EnvironmentService with getCurrentPeriod

---
+++ Development/BE/tcrms/src/main/java/com/kutca/tcrms/common/environment/service/EnvironmentService.java
@@ -0,0 +1,38 @@
+package com.kutca.tcrms.common.environment.service;
+
+import com.kutca.tcrms.common.enums.DatePeriod;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class EnvironmentService {
+
+    private final LocalDate firstPeriodStart = LocalDate.of(2024, 9,10);
+    private final LocalDate firstPeriodEnd = LocalDate.of(2024, 9,25);
+    private final LocalDate secondPeriodStart = LocalDate.of(2024, 10,10);
+    private final LocalDate secondPeriodEnd = LocalDate.of(2024, 10,25);
+    private final LocalDate competitionPeriodStart = LocalDate.of(2024, 11,1);
+    private final LocalDate competitionPeriodEnd = LocalDate.of(2024, 11,5);
+
+    public String getCurrentPeriod(){
+
+        LocalDate today = LocalDate.now();
+
+        if(isWithinPeriod(today, firstPeriodStart, firstPeriodEnd))
+            return DatePeriod.FIRST.name();
+
+        if(isWithinPeriod(today, secondPeriodStart, secondPeriodEnd))
+            return DatePeriod.SECOND.name();
+
+        if(isWithinPeriod(today, competitionPeriodStart, competitionPeriodEnd))
+            return DatePeriod.COMPETITION.name();
+
+        return DatePeriod.NONE.name();
+    }
+
+    private boolean isWithinPeriod(LocalDate date, LocalDate start, LocalDate end){
+        return (date.isEqual(start) || date.isAfter(start)) && (date.isBefore(end) || date.isEqual(end));
+    }
+
+}
