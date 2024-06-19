4285d613b5eb45b99aa57f4e23b87281eeb6d13c
Han7sunny <xoa1235@naver.com>
2024-06-19 17:34:48 +0900

    [BE][Feat]: Update FinalSubmitController with updateUserInfo

--- Development/BE/tcrms/src/main/java/com/kutca/tcrms/user/controller/FinalSubmitController.java
+++ Development/BE/tcrms/src/main/java/com/kutca/tcrms/user/controller/FinalSubmitController.java
@@ -4,6 +4,7 @@ import com.kutca.tcrms.account.service.AccountService;
 import com.kutca.tcrms.common.dto.response.ResponseDto;
 import com.kutca.tcrms.participantapplication.service.ParticipantApplicationService;
 import com.kutca.tcrms.universityapplication.service.UniversityApplicationService;
+import com.kutca.tcrms.user.controller.dto.request.UserRequestDto;
 import com.kutca.tcrms.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -13,9 +14,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -69,6 +68,18 @@ public class FinalSubmitController {
     public ResponseEntity<?> getUserInfo(@RequestParam Long userId){
         try {
             return new ResponseEntity<>(userService.getUserInfo(userId), HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Operation(summary = "대표자 정보 갱신", description = "대표자의 연락처, 입금자명 저장")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+    @PostMapping("/api/user/after-final-submit/user-info")
+    public ResponseEntity<?> updateUserInfo(@RequestBody UserRequestDto.Info userInfoRequestDto){
+        try {
+            return new ResponseEntity<>(userService.updateUserInfo(userInfoRequestDto), HttpStatus.OK);
         }
         catch (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
